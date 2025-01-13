00c59851be2604bdd949b42e3686cb8485ac3205
tb-rules10 <tanishq.bhatt10@gmail.com>
2025-01-13 12:37:46 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+export default withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  rules: {
+    "no-unused-vars": "off"
+  }
+})
